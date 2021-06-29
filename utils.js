1acd97084d45d61a53e1df976afae74e0aca24a2
786shuklaboi <46483575+786shuklaboi@users.noreply.github.com>
2021-06-29 09:19:20 +0530

    Add files via upload

--- utils.js
+++ utils.js
@@ -3,68 +3,68 @@ const { response } = require('express');
 const puppeteer = require('puppeteer');
 
 
-// async function getResponseStatus(url){
-//     const config = {
-//         method: 'GET',
-//         // url: 'https://www.vperfumes.com/products/collectihshddbonproducts-listing/sale-upto-75/605b8afadeab490012e8b1f6'
-//         url: url
-//     }
+async function getResponseStatus(url){
+    const config = {
+        method: 'GET',
+        // url: 'https://www.vperfumes.com/products/collectihshddbonproducts-listing/sale-upto-75/605b8afadeab490012e8b1f6'
+        url: url
+    }
 
-//     let res = await axios(config)
+    let res = await axios(config)
 
-//     return res.status
+    return res.status
 
-// }
-// exports.getResponseStatus = getResponseStatus;
+}
+exports.getResponseStatus = getResponseStatus;
 
-// async function getResponseTime(url){
-//     const config = {
-//         method: 'GET',
-//         url: url
-//     }
+async function getResponseTime(url){
+    const config = {
+        method: 'GET',
+        url: url
+    }
 
 
-//     const instance = axios.create()
-//     instance.interceptors.request.use((config) => {
-//         config.headers['request-startTime'] = process.hrtime()
-//         return config
-//     })
+    const instance = axios.create()
+    instance.interceptors.request.use((config) => {
+        config.headers['request-startTime'] = process.hrtime()
+        return config
+    })
 
-//     instance.interceptors.response.use((response) => {
-//         const start = response.config.headers['request-startTime']
-//         const end = process.hrtime(start)
-//         const milliseconds = Math.round((end[0] * 1000) + (end[1] / 1000000))
-//         response.headers['request-duration'] = milliseconds
-//         return response
-//     })
+    instance.interceptors.response.use((response) => {
+        const start = response.config.headers['request-startTime']
+        const end = process.hrtime(start)
+        const milliseconds = Math.round((end[0] * 1000) + (end[1] / 1000000))
+        response.headers['request-duration'] = milliseconds
+        return response
+    })
 
-//     // return parseInt(response)
+    // return parseInt(response)
 
-//     return instance.get(url).then((response) => {
-//         console.log(response.headers['request-duration'])
-//         return parseInt(response)
+    return instance.get(url).then((response) => {
+        console.log(response.headers['request-duration'])
+        return parseInt(response)
 
-//     }).catch((error) => {
-//         console.error(`Error`)
+    }).catch((error) => {
+        console.error(`Error`)
 
-//     })
-//     // return parseInt(response)
+    })
+    // return parseInt(response)
 
-// }
-// exports.getResponseTime = getResponseTime;
+}
+exports.getResponseTime = getResponseTime;
 
-// async function getResponseSize(url){
-//     const config = {
-//         method: 'GET',
-//         url: url
-//     }
+async function getResponseSize(url){
+    const config = {
+        method: 'GET',
+        url: url
+    }
 
-//     let res = await axios(config)
+    let res = await axios(config)
 
-//     return parseInt(res.length)
+    return parseInt(res.length)
 
-// }
-// exports.getResponseSize = getResponseSize;
+}
+exports.getResponseSize = getResponseSize;
 
 (async () => {
     const browser = await puppeteer.launch();
